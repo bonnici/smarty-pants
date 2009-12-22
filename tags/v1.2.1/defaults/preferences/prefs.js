86d5f3f199272343095ff1a8941ba23ac681ea03
nickmarsh <nickmarsh@76db3d3e-3ec3-11de-a81c-fde7d73ceb89>
2009-12-22 07:29:23 +0000

    Tagging v1.2.1

---
+++ tags/v1.2.1/defaults/preferences/prefs.js
@@ -0,0 +1,12 @@
+/*
+pref("extensions.smarty-pants.song-limit", 50);
+pref("extensions.smarty-pants.time-limit", 0);
+pref("extensions.smarty-pants.show-advanced", false);
+pref("extensions.smarty-pants.show-details", true);
+pref("extensions.smarty-pants.ignore-duplicate", true);
+pref("extensions.smarty-pants.auto-correct-source", true);
+pref("extensions.smarty-pants.auto-correct-results", true);
+pref("extensions.smarty-pants.score-cutoff", 0.15);
+pref("extensions.smarty-pants.sim-track-weight", 1);
+pref("extensions.smarty-pants.recommendation-filter", 0);
+*/
